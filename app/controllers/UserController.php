fc90c3658fd827550c35fa66f973faf0db100ac1
Niklas Andreasson <eaglus_@hotmail.com>
2013-12-16 19:35:00 +0100

    Worked on auth

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -6,16 +6,24 @@ class UserController extends BaseController {
 	{
 		$validator = Validator::make(Input::all(), User::$rules);
 
-   		if ($validator->passes()) {
+   		if ( $validator->passes() ) {
 			$user = new User();
-   			$user->firstname = Input::get('firstname');
-   			$user->lastname = Input::get('lastname');
    			$user->email = Input::get('email');
-   			$user->password = Hash::make(Input::get('password'));
+   			$user->password = Hash::make( Input::get('password') );
    			$user->save();
    		} else {
       		// validation has failed, display error messages
    		}
+	}
+
+	public function postSignin()
+	{
+		$userCredentials = Input::only('email', 'password');
+
+		if ( Auth::attempt( $userCredentials ) ) {
+			return Redirect::intended('/quiz');
+		}
+		return Redirect::to('/')->with('message', 'Your username/password combination was incorrect')->withInput();
 	}
 
 }
