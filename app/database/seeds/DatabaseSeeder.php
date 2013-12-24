d662354cc6ba5e9ea6c0f890b53ab5ba77ef8d30
Niklas Andreasson <eaglus_@hotmail.com>
2013-12-24 19:26:55 +0100

    Added User seeder

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -11,25 +11,42 @@ class DatabaseSeeder extends Seeder {
 	{
 		Eloquent::unguard();
 
+		$this->call('UserTableSeeder');
 		$this->call('QuizTableSeeder');
 		$this->call('QuestionTableSeeder');
 	}
 
 }
 
+class UserTableSeeder extends Seeder {
+
+	public function run()
+	{
+		DB::table('users')->delete();
+
+		User::create( array( 'email' => 'john@doe.com', 'password' => Hash::make('doe123') ) );
+	}
+}
+
 class QuizTableSeeder extends Seeder {
 
-	public function run() {
+	public function run()
+	{
 		DB::table('quizzes')->delete();
 
-		Quiz::create( array( 'title' => 'Heroes', 'desc' => 'Time to prove what you know about heroes!', 'status' => 'unpublished') );
+		$user = User::first();
+
+		Quiz::create( array( 'user_id' => $user->id, 'title' => 'Heroes', 'desc' => 'Time to prove what you know about heroes!', 'status' => 'unpublished') );
 	}
 
 }
 
 class QuestionTableSeeder extends Seeder {
 
-	public function run() {
+	public function run()
+	{
+		DB::table('questions')->delete();
+
 		$quiz = Quiz::first();
 
 		Question::create(
