83d238798b1484229339d25b1af8e8fb5bcf42bf
Niklas Andreasson <eaglus_@hotmail.com>
2013-12-30 18:23:29 +0100

    Created boilerplate code for UserControllerTest

---
+++ app/tests/UserControllerTest.php
@@ -0,0 +1,24 @@
+<?php
+
+class UserControllerTest extends TestCase {
+
+	public function __construct()
+	{
+
+	}
+
+	public function tearDown()
+	{
+
+	}
+
+	public function testPostCreate()
+	{
+
+	}
+
+	public function testPostSignin()
+	{
+
+	}
+}
